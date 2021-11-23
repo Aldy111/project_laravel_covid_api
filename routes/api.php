208674a65e1267022d72eb0e0f7cd3ac58872a7a
Aldy111 <aldyprayogo53@gmail.com>
2021-11-23 15:14:59 +0700

    penambahan auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientsController;
@@ -19,7 +20,15 @@ use App\Models\Patients;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('patients', PatientsController::class);
+    Route::get('/patients/status/{status}', [PatientsController::class, 'status']);
+    Route::get('patients/search/{name}', [PatientsController::class, 'search']);
+});
 
-Route::apiResource('patients', PatientsController::class);
-Route::get('/patients/status/{status}', [PatientsController::class, 'status']);
-Route::get('patients/search/{name}', [PatientsController::class, 'search']);
+
+
+#endpoint register dan login
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
